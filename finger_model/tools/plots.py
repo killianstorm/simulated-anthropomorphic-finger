c87539a998e02492e7a83cc96877707aa117dcc9
Killian Storm <killianstorm@gmail.com>
2020-05-12 11:59:02 +0200

    Extra plotting

--- finger_model/tools/plots.py
+++ finger_model/tools/plots.py
@@ -99,9 +99,6 @@ def rotate(point, origin, angle):
 def animation(history, dt, name=None, history2=None, tendons=False):
     key_points = history['positions']
 
-    if tendons:
-        # torques = history['torques']
-        angles = history['angles']
 
     # print(key_points)
 
@@ -115,10 +112,12 @@ def animation(history, dt, name=None, history2=None, tendons=False):
 
         plt.cla()
 
-        plt.plot(key_points[:4, i], key_points[4:, i], marker='.')
+        plt.plot(key_points[:4, i], key_points[4:, i], marker='.', linewidth=5, markersize=5)
         plt.scatter(history['end_effector'][0][:i], history['end_effector'][1][:i], s=0.1)
 
         if tendons:
+            torques = history['torques']
+            angles = history['angles']
 
             alpha1 = (num.pi / 2 + angles[i, 0])
             alpha2 = num.pi - (angles[i, 0] - angles[i, 1])
@@ -128,45 +127,51 @@ def animation(history, dt, name=None, history2=None, tendons=False):
             # MCP
             x, y = key_points[0, i], key_points[4, i]
             mcp_x, mcp_y = rotate((x, y - RADII[J_MCP][T_FS]), (x, y), - (num.pi / 2 - angles[i, 0]))
-            ax.add_patch(patches.Circle((x, y), radius=RADII[J_MCP][T_FS], color=FS_COLOR, linewidth=0.1, fill=False))
+            ax.add_patch(patches.Circle((x, y), radius=RADII[J_MCP][T_FS], color=FS_COLOR, linewidth=0.1))
 
             # PIP
             x, y = key_points[1, i], key_points[5, i]
-            ax.add_patch(patches.Circle((x, y), radius=RADII[J_PIP][T_FS], color=FS_COLOR, linewidth=0.1, fill=False))
+            ax.add_patch(patches.Circle((x, y), radius=RADII[J_PIP][T_FS], color=FS_COLOR, linewidth=0.1))
             pip_x, pip_y = rotate((x, y - RADII[J_PIP][T_FS]), (x, y), -(num.pi / 2 - angles[i, 1]))
 
-            plt.plot([-0.1, mcp_x, pip_x], [-RADII[J_MCP][T_FS], mcp_y, pip_y], color=FS_COLOR)
+            plt.plot([-0.2, -0.1, mcp_x, pip_x], [-0.1, -RADII[J_MCP][T_FS], mcp_y, pip_y], color=FS_COLOR)
 
-            ### IO tendon ###
-            # MCP
-            x, y = key_points[0, i], key_points[4, i]
-            mcp_x, mcp_y = rotate((x, y - RADII[J_MCP][T_IO]), (x, y), - (num.pi / 2 - angles[i, 0]))
-            ax.add_patch(patches.Circle((x, y), radius=RADII[J_MCP][T_IO], color=IO_COLOR, linewidth=0.1, fill=False))
-
-            # PIP
-            x, y = key_points[1, i], key_points[5, i]
-            ax.add_patch(patches.Circle((x, y), radius=RADII[J_PIP][T_IO], color=IO_COLOR, linewidth=0.1, fill=False))
-            pip_x, pip_y = rotate((x, y + RADII[J_PIP][T_IO]), (x, y), -(num.pi / 2 - angles[i, 1]))
-
-            plt.plot([-0.1, mcp_x, pip_x], [-RADII[J_MCP][T_IO], mcp_y, pip_y], color=IO_COLOR)
+            plt.text(-0.25, -0.125, 'FS: ' + str(round(torques[i, 0], 2)))
 
             ### FP tendon ###
             # MCP
             x, y = key_points[0, i], key_points[4, i]
             mcp_x, mcp_y = rotate((x, y - RADII[J_MCP][T_FP]), (x, y), - (num.pi / 2 - angles[i, 0]))
-            ax.add_patch(patches.Circle((x, y), radius=RADII[J_MCP][T_FP], color=FP_COLOR, linewidth=0.1, fill=False))
+            ax.add_patch(patches.Circle((x, y), radius=RADII[J_MCP][T_FP], color=FP_COLOR, linewidth=0.1))
 
             # PIP
             x, y = key_points[1, i], key_points[5, i]
-            ax.add_patch(patches.Circle((x, y), radius=RADII[J_PIP][T_FP], color=FP_COLOR, linewidth=0.1, fill=False))
+            ax.add_patch(patches.Circle((x, y), radius=RADII[J_PIP][T_FP], color=FP_COLOR, linewidth=0.1))
             pip_x, pip_y = rotate((x, y - RADII[J_PIP][T_FP]), (x, y), -(num.pi / 2 - angles[i, 1]))
 
             # DIP
             x, y = key_points[2, i], key_points[6, i]
-            ax.add_patch(patches.Circle((x, y), radius=RADII[J_DIP][T_FP], color=FP_COLOR, linewidth=0.1, fill=False))
+            ax.add_patch(patches.Circle((x, y), radius=RADII[J_DIP][T_FP], color=FP_COLOR, linewidth=0.1))
             dip_x, dip_y = rotate((x, y - RADII[J_DIP][T_FP]), (x, y), -(num.pi / 2 - angles[i, 1]))
 
-            plt.plot([-0.1, mcp_x, pip_x, dip_x], [-RADII[J_MCP][T_FP], mcp_y, pip_y, dip_y], color=FP_COLOR)
+            plt.plot([-0.1, -0.1, mcp_x, pip_x, dip_x], [-0.1, -0.05, mcp_y, pip_y, dip_y], color=FP_COLOR)
+            plt.text(-0.125, -0.125, 'FP: ' + str(round(torques[i, 2], 2)))
+
+            ### IO tendon ###
+            # MCP
+            x, y = key_points[0, i], key_points[4, i]
+            mcp_x, mcp_y = rotate((x, y - RADII[J_MCP][T_IO]), (x, y), - (num.pi / 2 - angles[i, 0]))
+            ax.add_patch(patches.Circle((x, y), radius=RADII[J_MCP][T_IO], color=IO_COLOR, linewidth=0.1))
+
+            # PIP
+            x, y = key_points[1, i], key_points[5, i]
+            ax.add_patch(patches.Circle((x, y), radius=RADII[J_PIP][T_IO], color=IO_COLOR, linewidth=0.1))
+            pip_x, pip_y = rotate((x, y + RADII[J_PIP][T_IO]), (x, y), -(num.pi / 2 - angles[i, 1]))
+
+            plt.plot([-0.2, -0.1, mcp_x, pip_x], [-RADII[J_MCP][T_IO], -RADII[J_MCP][T_IO], mcp_y, pip_y], color=IO_COLOR)
+
+            plt.text(-0.25, -RADII[J_MCP][T_IO] - 0.025, 'IO: ' + str(round(torques[i, 1], 2)))
+
 
             ### ED tendon ###
             # MCP
@@ -184,7 +189,9 @@ def animation(history, dt, name=None, history2=None, tendons=False):
             ax.add_patch(patches.Circle((x, y), radius=RADII[J_DIP][T_ED], color=ED_COLOR, linewidth=0.1, fill=False))
             dip_x, dip_y = rotate((x, y + RADII[J_DIP][T_ED]), (x, y), -(num.pi / 2 - angles[i, 1]))
 
-            plt.plot([-0.1, mcp_x, pip_x, dip_x], [RADII[J_MCP][T_ED], mcp_y, pip_y, dip_y], color=ED_COLOR)
+            plt.plot([-0.2, -0.1, mcp_x, pip_x, dip_x], [RADII[J_MCP][T_ED], RADII[J_MCP][T_ED], mcp_y, pip_y, dip_y], color=ED_COLOR)
+
+            plt.text(-0.25, -RADII[J_MCP][T_ED] + 0.025, 'ED: ' + str(round(torques[i, 3], 2)))
 
 
 
