b6fbc70aa82bf811959e973d2b8f3bac9522573b
Killian Storm <killianstorm@gmail.com>
2020-06-13 00:18:24 +0200

    Piano key notebook

--- finger_model/analysis/learning/tendon_model/loaded/keystroke/learn_keystroke_angle_loss.py
+++ finger_model/analysis/learning/tendon_model/loaded/keystroke/learn_keystroke_angle_loss.py
@@ -1,18 +1,17 @@
 from analysis.learning.gradient_descent import *
 import pickle
 
-# Precision erros after 144 iterations. Loss = 0.84501
+# Precision errors after 144 iterations. Loss = 0.84501
 
 # Interval.
 tmax, dt = 2., 0.001
 interval = num.arange(0, tmax + dt, dt)
 
-name = "loaded piano keystroke trajectory \n with angle loss function"
-
 infile = open("full_piano_keystroke.pickle",'rb')
 reference = pickle.load(infile)
 infile.close()
 
+name = "loaded piano keystroke trajectory \n with angle loss function"
 loss_function = loss_angles
 
 # Learn to reproduce trajectory using gradient descent.
